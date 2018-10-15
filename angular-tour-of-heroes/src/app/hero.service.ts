a0c5cb5509f9f437a4d9bfa9d4e5f6a3f7157812
mwhorse46 <needmefordev@gmail.com>
2018-10-15 22:01:39 +0530

    add method for heroes

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+	getHeroes(): Hero[] {
+		return HEROES;
+	}
 
   constructor() { }
 }
