b80c8a42f521fa51f922b3ec0b58b88368806451
mwhorse46 <needmefordev@gmail.com>
2018-10-15 10:55:45 +0530

    create mock heroes server

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ 'id': 11, name: 'Iron Man' },
+	{ 'id': 12, name: 'Captain America' },
+	{ 'id': 13, name: 'Hulk' },
+	{ 'id': 14, name: 'Narco' },
+	{ 'id': 15, name: 'Directory Fury' },
+	{ 'id': 16, name: 'SHIELD' },
+	{ 'id': 17, name: 'Flash' },
+	{ 'id': 18, name: 'JARVIS' },
+	{ 'id': 19, name: 'Thor' },
+	{ 'id': 20, name: 'Loki' },
+	{ 'id': 21, name: 'Dr. Banners' },
+	{ 'id': 22, name: 'Iron Fist' },
+]
