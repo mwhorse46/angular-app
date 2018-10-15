e2f21e8b94343577b766ae4231400d2afeea0205
mwhorse46 <needmefordev@gmail.com>
2018-10-15 09:51:16 +0530

    make hero object

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -12,5 +13,8 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
-  hero = 'Windstorm';
+  hero: Hero = {
+  	id: 1,
+  	name: 'Windstorm'
+  };
 }
