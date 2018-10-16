aa8a61c418cda4f9df923124fc14c2301086f577
mwhorse46 <needmefordev@gmail.com>
2018-10-16 15:06:35 +0530

    add dashboard url in routers

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-	{ path: 'heroes', component: HeroesComponent }
+	{ path: 'heroes', component: HeroesComponent },
+	{ path: 'dashboard', component: DashboardComponent },
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
 
 @NgModule({
