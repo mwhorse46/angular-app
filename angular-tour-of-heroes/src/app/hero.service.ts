f5b9e27d46b464c077a3b334eca8e5389b16b57c
mwhorse46 <needmefordev@gmail.com>
2018-10-16 09:28:37 +0530

    use observable async approach

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-	getHeroes(): Hero[] {
-		return HEROES;
+	getHeroes(): Observable<Hero[]> {
+		return of(HEROES);
 	}
 
   constructor() { }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -21,7 +21,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   heroes: Hero[];
