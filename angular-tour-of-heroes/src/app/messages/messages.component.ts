c077975bc8671f6421061fc83817630b3806ab8b
mwhorse46 <needmefordev@gmail.com>
2018-10-16 09:56:10 +0530

    add messages functionality

--- angular-tour-of-heroes/src/app/messages/messages.component.ts
+++ angular-tour-of-heroes/src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public messageService: MessageService) { }
 
   ngOnInit() {
   }
