ec1d532ebd6ecad39ba4aefc3d8ce63086fc32a4
mwhorse46 <needmefordev@gmail.com>
2018-10-16 18:47:25 +0530

    add method to fetch hero with id in url

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,9 +12,20 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  	private route: ActivatedRoute,
+  	private heroService: HeroService,
+  	private location: Location
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+  	this.getHero();
+  }
+
+  getHero(): void {
+  	const id = +this.route.snapshot.paramMap.get('id');
+  	this.heroService.getHero(id)
+  		.subscribe(hero => this.hero = hero);
   }
 
   @Input() hero: Hero;
