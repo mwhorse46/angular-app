5a65e0c3d3acc6123bfd2e1555a77b1119c619bc
mwhorse46 <needmefordev@gmail.com>
2018-10-16 11:15:56 +0530

    define routes for hero component

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+	{ path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-	exports: [ RouterModule ]
+	exports: [ RouterModule ],
+	imports: [ RouterModule.forRoot(routes) ]
 })
 export class AppRoutingModule { }
