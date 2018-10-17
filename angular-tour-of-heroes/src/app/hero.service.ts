37f3e2542c4f834375edeab9465e6c41a0a42519
mwhorse46 <needmefordev@gmail.com>
2018-10-17 13:38:46 +0530

    add error handling in hero service

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -15,7 +16,10 @@ export class HeroService {
 		// TODO: send the message __after__ fetching the heroes
 		this.messageService.add('HeroService: fetched heroes');
 		// return of(HEROES);
-		return this.http.get<Hero[]>(this.heroesUrl);
+		return this.http.get<Hero[]>(this.heroesUrl)
+			.pipe(
+				catchError(this.handleError('getHeroes', []))
+			)
 	}
 
 	getHero(id: number): Observable<Hero> {
@@ -23,6 +27,26 @@ export class HeroService {
 		this.messageService.add(`HeroService: fetched hero id = ${id}`);
 		return of(HEROES.find(hero => hero.id === id));
 	}
+
+	    /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+    private handleError<T> (operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+     
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+     
+        // TODO: better job of transforming error for user consumption
+        this.log(`${operation} failed: ${error.message}`);
+     
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+      };
+    }
 
 	/** Log a HeroService message with the MessageService */
 	private log(message: string) {
